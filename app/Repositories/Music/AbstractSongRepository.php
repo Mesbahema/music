19d972a1ba3a6f5f7c0edb993674743ca1cf2193
Mesbah <you@example.comemamim612@gmail.com>
2019-12-11 09:39:28 +0330

    Repository commit initial

---
+++ app/Repositories/Music/AbstractSongRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Repositories\Music;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AbstractSongRepository extends Model
+{
+    //
+}
---
+++ app/Repositories/Music/MysqlSongRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Repositories\Music;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MysqlSongRepository extends Model
+{
+    //
+}
